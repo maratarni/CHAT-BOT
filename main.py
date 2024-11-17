62a53a4aaabdc4dafdfd97502c99bdefc3a5bd27
maratarni <mara.tarniceru@stud.acs.upb.ro>
2024-11-17 11:20:13 +0200

    i did it, am clonat la mn branchul lui alin
    yuhuuuu

--- main.py
+++ main.py
@@ -2,7 +2,34 @@ from docx import Document
 import os
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+import eel
+import sys
 
+import ctypes
+
+lib = ctypes.CDLL("./test.dll")
+
+# Definește semnăturile funcțiilor
+lib.test.argtypes = [ctypes.c_int, ctypes.c_int]
+lib.test.restype = ctypes.c_int  # Returnează un C-string
+lib.date.restype = ctypes.c_char_p
+
+# Apelează funcția
+result = lib.date()  # Obține data ca un C-string
+print(result.decode('utf-8'))
+
+def get_resource_path(relative_path):
+    """Găsește calea către resurse în cazul executabilului PyInstaller."""
+    if hasattr(sys, '_MEIPASS'):
+        # PyInstaller stochează resursele în _MEIPASS
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+# Initializam un server eel cu pagina web din folderul 'web'
+docx_path1 = get_resource_path('raspunsuri.docx')
+docx_path2 = get_resource_path('intrebari.docx')
+web_folder = get_resource_path('web')
+eel.init(web_folder)
 
 
 #imi citeste datele din word daca poate si daca nu imi zice ca nu poate
@@ -25,8 +52,8 @@ def creeaza_dictionar_raspunsuri(cale_intrebari, cale_raspunsuri):
         return {}
 
     # Citește întrebările și răspunsurile
-    intrebari = citeste_word("C:/FACULTATE/futuo/intrebari.docx")
-    raspunsuri = citeste_word("C:/FACULTATE/futuo/raspunsuri.docx")
+    intrebari = citeste_word(docx_path2)
+    raspunsuri = citeste_word(docx_path1)
 
     # Verifică dacă numărul de întrebări și răspunsuri este egal
     if len(intrebari) != len(raspunsuri):
@@ -60,14 +87,15 @@ def find_best_matches(user_input, intrebari, threshold=70):
     return matches
 
 
-def chatbot():
+@eel.expose
+def chatbot(user_input):
 
     # Specificați căile către fișierele Word
-    intrebari = citeste_word("C:/FACULTATE/futuo/intrebari.docx")
-    raspunsuri = citeste_word("C:/FACULTATE/futuo/raspunsuri.docx")
+    intrebari = citeste_word(docx_path2)
+    raspunsuri = citeste_word(docx_path1)
 
     # Creează dicționarul de răspunsuri
-    responses = creeaza_dictionar_raspunsuri("C:/FACULTATE/futuo/intrebari.docx", "C:/FACULTATE/futuo/raspunsuri.docx")
+    responses = creeaza_dictionar_raspunsuri(docx_path2, docx_path1)
 
     if not responses:
         print("Nu s-a putut initializa chatbot-ul!")
@@ -76,7 +104,7 @@ def chatbot():
     print("Chatbot inițializat! Tastează 'exit' pentru a ieși.")
 #aici e magia
     while True:
-        user_input = input("Tu: ").lower()
+        # user_input = input("Tu: ").lower()
 
         if user_input == 'exit':
             print("La revedere!")
@@ -90,10 +118,15 @@ def chatbot():
             best_match_question = matching_questions[0][0]  # Prima potrivire
             index = intrebari.index(best_match_question)
             raspuns = raspunsuri[index]
+        elif user_input == 'data':
+            raspuns = result.decode('utf-8')
         else:
             raspuns = "Îmi pare rău, nu știu să răspund la această întrebare."
 
-        print("Chatbot:", raspuns)
+        # print("Chatbot:", raspuns)
+        return raspuns
 
-chatbot()
+# chatbot()
 
+eel.start('index.html', size=(1920, 1080))
+# am modificat in new branch
